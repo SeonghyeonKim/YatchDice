51815fc479cf9614f5cfe8b0190e975514900267
SeonghyeonKim <kkgojn@gmail.com>
2025-01-09 16:04:57 +0900

    Init Backend Project

---
+++ Yatch-back/src/main/java/com/yatch/Yatch_back/YatchBackApplication.java
@@ -0,0 +1,13 @@
+package com.yatch.Yatch_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatchBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YatchBackApplication.class, args);
+	}
+
+}
